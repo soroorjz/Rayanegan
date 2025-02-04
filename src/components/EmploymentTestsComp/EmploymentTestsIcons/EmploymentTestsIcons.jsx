6b1d8418836274fefa530786d9cfd179034c1d89
SoroorJameizadeh <soroorjameizadeh@gmail.com>
2025-02-04 10:34:11 +0330

    EmploymentTests done - responsive

--- src/components/EmploymentTestsComp/EmploymentTestsIcons/EmploymentTestsIcons.jsx
+++ src/components/EmploymentTestsComp/EmploymentTestsIcons/EmploymentTestsIcons.jsx
@@ -6,25 +6,35 @@ import { MdOutlineDownloadDone } from "react-icons/md";
 import { HiOutlineSpeakerphone } from "react-icons/hi";
 import { BiSolidNoEntry } from "react-icons/bi";
 const EmploymentTestsIcons = () => {
+  const handleScroll = (targetId) => {
+    const element = document.getElementById(targetId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="EmploymentTestsIcons-Container">
-      <button className="Registering">
+      <button
+        className="Registering"
+        onClick={() => handleScroll("Registering")}
+      >
         <LuClipboardPenLine />
         درحال نام‌نویسی{" "}
       </button>
-      <button className="InProgress">
+      <button className="InProgress" onClick={() => handleScroll("InProgress")}>
         <FaHourglassHalf />
         در جریان{" "}
       </button>
-      <button className="Active">
+      <button className="Active" onClick={() => handleScroll("Active")}>
         <MdOutlineDownloadDone />
         فعال{" "}
       </button>
-      <button className="Announcing">
+      <button className="Announcing" onClick={() => handleScroll("Announcing")}>
         <HiOutlineSpeakerphone />
         درحال اعلام نتایج
       </button>
-      <button className="Expired">
+      <button className="Expired" onClick={() => handleScroll("Expired")}>
         <BiSolidNoEntry />
         منقضی شده
       </button>
--- src/pages/EmploymentTests/EmploymentTests.jsx
+++ src/pages/EmploymentTests/EmploymentTests.jsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./EmploymentTests.scss";
+import { IoMdHome } from "react-icons/io";
 import EmploymentTestsComp from "../../components/EmploymentTestsComp/EmploymentTestsComp";
 import EmploymentTestsIcons from "../../components/EmploymentTestsComp/EmploymentTestsIcons/EmploymentTestsIcons";
 import NavbarTop from "../../components/HomePageComp/NavbarTop/NavbarTop";
+import { activeExams } from "./EmploymentTestsData";
+import EmploymentTestsBanner from "../../components/EmploymentTestsComp/EmploymentTestsBanner/EmploymentTestsBanner";
+import { Link } from "react-router";
 const EmploymentTests = () => {
   return (
     <div className="EmploymentTests">
       <NavbarTop />
+      <div className="EmploymentTestsBanner">
+        <EmploymentTestsBanner />
+      </div>
       <div className="EmploymentTestsIcons">
         <EmploymentTestsIcons />
       </div>
-
-      <div className="EmploymentTestsComp">
-        <EmploymentTestsComp />
+      <div id="Registering" className="EmploymentTestsComp">
+        <EmploymentTestsComp
+          examData={activeExams}
+          title="آزمون‌های درحال نام‌نویسی "
+        />
       </div>
+      <div id="InProgress" className="EmploymentTestsComp">
+        <EmploymentTestsComp
+          examData={activeExams}
+          title="آزمون‌های در جریان"
+        />
+      </div>
+      <div id="Active" className="EmploymentTestsComp">
+        <EmploymentTestsComp examData={activeExams} title="آزمون‌های فعال" />
+      </div>
+      <div id="Announcing" className="EmploymentTestsComp">
+        <EmploymentTestsComp
+          examData={activeExams}
+          title="آزمون‌های در حال اعلام نتایج"
+        />
+      </div>
+      <div id="Expired" className="EmploymentTestsComp expiredExams">
+        <EmploymentTestsComp
+          examData={activeExams}
+          title="آزمون‌های منقضی شده   "
+        />
+      </div>
+      <button className="homeBtn">
+        <Link to="/">
+          <IoMdHome />
+        </Link>
+      </button>
     </div>
   );
 };
