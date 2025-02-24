a3787555763bec7b4bf952beb714c9ba3b1fe0f2
SoroorJameizadeh <soroorjameizadeh@gmail.com>
2025-02-24 11:50:58 +0330

    FaqHeader swall added

--- src/components/HomePageComp/Faq/FaqHeader/FaqHeader.jsx
+++ src/components/HomePageComp/Faq/FaqHeader/FaqHeader.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
 import "./FaqHeader.scss";
+import { useNavigate } from "react-router-dom";
 import Faq from "../Faq";
 import SupportHelpCenter from "../SupportHelpCenter/SupportHelpCenter";
 import { useAuth } from "../../../../AuthContext";
+import Swal from "sweetalert2";
 const FaqHeader = () => {
   const { user } = useAuth(); // دریافت اطلاعات کاربر
+  const navigate = useNavigate();
   const [isFaqSelected, setIsFaqSelected] = useState(true);
-
   const handleToggle = () => {
     if (!user) {
-      alert("برای استفاده از پشتیبانی آفلاین لطفاً وارد حساب خود شوید.");
+      Swal.fire({
+        // title: "ورود به حساب کاربری",
+        text: "برای استفاده از پشتیبانی آفلاین لطفاً وارد حساب کاربری خود شوید.",
+        icon: "warning",
+        confirmButtonText: "ورود به حساب کاربری",
+        confirmButtonColor: "#3085d6",
+        showCancelButton: true,
+        cancelButtonText: "لغو",
+        customClass: {
+          confirmButton: "SupportHelpCenter-confirm-button",
+          cancelButton: "SupportHelpCenter-cancel-button",
+          popup: "SupportHelpCenter-custom-popup",
+        },
+        buttonsStyling: false,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/logIn");
+        }
+      });
       return;
     }
     setIsFaqSelected(!isFaqSelected);
   };
-
   return (
     <div className="faqHeaderContainer">
       <div className="toggle-switch-container">
@@ -23,10 +42,14 @@ const FaqHeader = () => {
           onClick={handleToggle}
           style={{ cursor: user ? "pointer" : "not-allowed" }} // دکمه غیرفعال شده در صورت عدم ورود
         >
-          <span id="faqOptionBtn" className={`option faq ${!isFaqSelected ? "inactive" : ""}`}>
+          <span
+            id="faqOptionBtn"
+            className={`option faq ${!isFaqSelected ? "inactive" : ""}`}
+          >
             سوالات متداول
           </span>
-          <span id="offlineSupportOptionBtn"
+          <span
+            id="offlineSupportOptionBtn"
             className={`option support ${isFaqSelected ? "inactive" : ""} ${
               !user ? "disabled" : ""
             }`}
