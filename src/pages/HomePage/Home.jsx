6b25d79a0179ad4fee889294522f29ac7d38efbc
SoroorJameizadeh <soroorjameizadeh@gmail.com>
2025-03-06 18:12:53 +0330

    pwa added

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import HomePage from "../../components/HomePageComp/HomePage";
+import PwaComp from "../PwaComp";
 
 const Home = () => {
-  return <HomePage />;
+  return (
+    <>
+      <HomePage />
+      <PwaComp />
+    </>
+  );
 };
 
 export default Home;
