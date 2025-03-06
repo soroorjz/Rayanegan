2d111c2a27a561371aea1384fe58c761aba21433
SoroorJameizadeh <soroorjameizadeh@gmail.com>
2025-03-06 21:06:51 +0330

    ExamFormResult btn fixed

--- src/components/HomePageComp/ExamForm/ExamFormResult/ExamFormResult.jsx
+++ src/components/HomePageComp/ExamForm/ExamFormResult/ExamFormResult.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import "./ExamFormResult.scss";
-import { Link } from "react-router";
+import { Link } from "react-router-dom"; // اصلاح وارد کردن Link
+import { useAuth } from "../../../../AuthContext"; // اضافه کردن useAuth برای دریافت وضعیت کاربر
 
 const exams = [
   {
     id: 1,
-    name:"دوازدهمین آزمون مشترک فراگیر دستگاه‌های اجرایی کشور",
+    name: "دوازدهمین آزمون مشترک فراگیر دستگاه‌های اجرایی کشور",
     jobLoc: "بانکدار گروه امور کامپیوتر و فناوری اطلاعات، البرز،کرج،قلم",
     num: "۱۰۲۴",
     date: "۱۴۰۴/۱/۲۵",
   },
   {
     id: 2,
-    name:"دوازدهمین آزمون مشترک فراگیر دستگاه‌های اجرایی کشور",
+    name: "دوازدهمین آزمون مشترک فراگیر دستگاه‌های اجرایی کشور",
     jobLoc: "بانکدار امور مالی، البرز، کرج، قلم",
     num: "۱۱۹۹",
     date: "۱۴۰۴/۲/۲۰",
   },
   {
     id: 3,
-    name:"دوازدهمین آزمون مشترک فراگیر دستگاه‌های اجرایی کشور",
+    name: "دوازدهمین آزمون مشترک فراگیر دستگاه‌های اجرایی کشور",
     jobLoc: "بانکدار امور مالی، البرز، کرج، گلشهر",
     num: "۱۲۰۰",
     date: "۱۴۰۴/۴/۱۰",
   },
 ];
+
 const ExamFormResult = () => {
+  const { user } = useAuth(); // دریافت وضعیت کاربر از AuthContext
+
   return (
     <div className="ResultExam-list">
       {exams.map((exam) => (
@@ -34,14 +38,20 @@ const ExamFormResult = () => {
           <span className="ResultExam-date">{exam.date}</span>
           <span className="ResultExam-date">{exam.num}</span>
           <span className="ResultExam-jobLoc">{exam.jobLoc}</span>
-          <a
-            className="ResultExam-more"
-            href={`/examInfo/${exam.name}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button>بیشتر</button>
-          </a>
+          <div className="ResultExam-more">
+            {user ? (
+              <Link to="/RegistrationPage">
+                <button>ثبت نام</button>
+              </Link>
+            ) : (
+              <div className="button-group">
+                <Link to="/logIn">
+                  <button>ورود به حساب کاربری</button>
+                </Link>
+                
+              </div>
+            )}
+          </div>
         </div>
       ))}
     </div>
