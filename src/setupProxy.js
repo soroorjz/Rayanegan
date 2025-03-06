08d649f83170f8e071a849bcee17dcd3ca46912f
SoroorJameizadeh <soroorjameizadeh@gmail.com>
2025-03-06 19:56:12 +0330

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
-    "/api",  // مسیر درخواست‌ها
+    "/api", // مسیر درخواست‌ها
     createProxyMiddleware({
-      target: "https://smp.devrayan.ir:2053", // سرور مقصد
+      target: "https://smp.devrayan.ir", // سرور مقصد
       changeOrigin: true,
       secure: false, // در صورت مشکل SSL
     })
